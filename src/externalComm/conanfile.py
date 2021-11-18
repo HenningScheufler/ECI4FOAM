55eb422fcc123dbf165be388bca060a3ad33ebbf
Scheufler, Henning <henning.scheufler@dlr.de>
2021-11-18 15:42:27 +0100

    WIP remvoed import from conan

--- src/externalComm/conanfile.py
+++ src/externalComm/conanfile.py
@@ -6,5 +6,5 @@ class fmu4foam(ConanFile):
     requires = ["cppzmq/4.7.1","nlohmann_json/3.9.1","OpenFOAMGen/0.2@myuser/OpenFOAMGen"]
     generators = "OpenFOAMGen"
 
-    def imports(self):
-        self.copy("*.hpp", "lnInclude", "include")
+    # def imports(self):
+    #     self.copy("*.hpp", "lnInclude", "include")
